12a32d05d19fad1e29bc4c9befea4f951291c6a9
andre-carbajal <carbajalvargasandre@gmail.com>
2023-12-02 15:36:35 -0500

    StringUtils was removed and code was added to Log

--- src/main/java/net/anvian/mineControlLauncher/util/Log.java
+++ src/main/java/net/anvian/mineControlLauncher/util/Log.java
@@ -1,6 +1,10 @@
 package net.anvian.mineControlLauncher.util;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class Log {
     private static final PrintStream consoleOutput = System.out;
@@ -19,11 +23,18 @@ public class Log {
 
     public static void error(String string, Throwable ex) {
         printer("[Mine Control Launcher] [ERROR] " + string);
-        printer("[Mine Control Launcher] [ERROR] " + StringUtils.getStackTrace(ex));
+        printer("[Mine Control Launcher] [ERROR] " + getStackTrace(ex));
     }
 
     private static void printer(String line) {
         consoleOutput.println(line);
     }
 
+    @NotNull
+    private static String getStackTrace(@NotNull Throwable throwable) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw, true);
+        throwable.printStackTrace(pw);
+        return sw.getBuffer().toString();
+    }
 }
