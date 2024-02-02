c324515711f673cf47926e7919763923220e0913
andre-carbajal <carbajalvargasandre@gmail.com>
2024-02-01 22:59:25 -0500

    application version check added

--- src/main/java/net/anvian/mineControlLauncher/util/checher/ApplicationChecker.java
+++ src/main/java/net/anvian/mineControlLauncher/util/checher/ApplicationChecker.java
@@ -1,5 +1,8 @@
 package net.anvian.mineControlLauncher.util.checher;
 
+import net.anvian.mineControlLauncher.gui.Gui;
+import net.anvian.mineControlLauncher.gui.GuiInstance;
+import net.anvian.mineControlLauncher.util.GithubApi;
 import net.anvian.mineControlLauncher.util.Log;
 import net.anvian.mineControlLauncher.util.download.ApplicationDownloader;
 
@@ -7,13 +10,29 @@ import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ApplicationChecker implements Checker {
+    private static final GithubApi githubApi = new GithubApi("MineControlFx");
+    private static final Pattern versionPattern = Pattern.compile("MineControlFx-(.*)\\.jar");
     public static final ApplicationDownloader downloader = new ApplicationDownloader();
+    private static final Gui guiInstance = GuiInstance.getInstance();
 
     @Override
     public void check(Path dirPath) throws IOException {
         if (!applicationExists(dirPath)) {
+            downloader.download();
+        }
+
+        String fileVersion = getFileVersion(dirPath);
+        String latestTag = githubApi.getTag();
+
+        if (fileVersion.equals(latestTag)) {
+            Log.println("Application is up to date");
+        } else if (Float.parseFloat(latestTag) > Float.parseFloat(fileVersion)) {
+            Log.println("Application is outdated");
+            deleteExistingVersion(dirPath);
             downloader.download();
         }
 
@@ -24,6 +43,35 @@ public class ApplicationChecker implements Checker {
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(dirPath, "MineControlFx*")) {
             return stream.iterator().hasNext();
         } catch (IOException e) {
+            Log.error("Error while checking for existing application");
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getFileVersion(Path dirPath) {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dirPath, "MineControlFx*")) {
+            for (Path entry : stream) {
+                Matcher matcher = versionPattern.matcher(entry.getFileName().toString());
+                if (matcher.find()) {
+                    return matcher.group(1);
+                }
+            }
+        } catch (IOException e) {
+            Log.error("Error while getting file version");
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    private void deleteExistingVersion(Path dirPath) {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dirPath, "MineControlFx*")) {
+            for (Path entry : stream) {
+                Files.delete(entry);
+                guiInstance.setLogLabel("Deleted outdated application: " + entry.getFileName());
+                Log.println("Deleted outdated application: " + entry.getFileName());
+            }
+        } catch (IOException e) {
+            Log.error("Error while deleting existing application");
             throw new RuntimeException(e);
         }
     }
